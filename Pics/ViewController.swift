d7f18327958b4464dd8c021818f5ffae71f72ba1
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-06-13 18:12:06 -0300

    Initial Commit

---
+++ Pics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pics
+//
+//  Created by Eduardo Vital Alencar Cunha on 13/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
